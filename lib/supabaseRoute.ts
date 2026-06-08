1b028c90ec4d00207b20ec625b2447584d815587
Parshuram Kumar <yadavparshuram991@gmail.com>
2026-06-08 11:54:00 +0530

    error deployement debug

--- lib/supabaseRoute.ts
+++ lib/supabaseRoute.ts
@@ -1,15 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function supabaseRoute() {
-  console.log(
-    "URL:",
-    process.env.NEXT_PUBLIC_SUPABASE_URL
-  );
-
-  console.log(
-    "NEXT_PUBLIC_SUPABASE_ANON_KEY:",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+ 
 
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
